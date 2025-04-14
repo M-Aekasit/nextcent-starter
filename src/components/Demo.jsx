b656430fcf3c8842594e9062d262bf0ee63e277c
aekasit <aekasit2002@gmail.com>
2025-04-14 16:56:18 +0700

    feat: add Demo component for showcasing demo call-to-action

---
+++ src/components/Demo.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { HiOutlineArrowNarrowRight } from "react-icons/hi";
+
+function Demo() {
+  return (
+    <div className='bg-[#F5F7FA] py-10 flex justify-center text-center'>
+        <div>
+            <h3 className='text-[#263238] text-[4rem] font-semibold  leading-[1]'>Pellentesque suscipit <br />fringilla libero eu.</h3>
+            <a href='#' className='inline-flex justify-center items-center mt-5 py-3 px-8  bg-[#4CAF4F] text-white rounded-md'>Get a Demo <HiOutlineArrowNarrowRight className='ml-2'/></a>
+        </div>
+      
+    </div>
+  )
+}
+
+export default Demo
